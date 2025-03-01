6fd80167b020a6a25e7ca9300ff0e2a436bd5bf8
qhoncheh <ghoncheh.ataee@gmail.com>
2025-03-01 12:10:31 +0330

    add languages in header and some another changes ✔️

--- src/components/panel/header/header.tsx
+++ src/components/panel/header/header.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Icons } from "./Icons/Icons";
 import UserProfile from "./user-profile/UserProfile";
+import ChangeLanguage from "../../language/languages";
+import { NotificationIconProps } from "./model";
 
-interface NotificationIconProps {
-  Icon: React.ComponentType<{ className: string }>;
-  count: number;
-  color: string;
-}
-
-const NotificationIcon: React.FC<NotificationIconProps> = ({ Icon, count, color }) => (
+const NotificationIcon: React.FC<NotificationIconProps> = ({
+  Icon,
+  count,
+  color,
+}) => (
   <div className="relative cursor-pointer">
     <Icon className={`w-5 h-5 text-${color}`} />
     {count > 0 && (
@@ -24,30 +24,28 @@ const Header: React.FC = () => {
     <header className="flex items-center justify-between px-6 py-3 bg-white shadow-sm h-full">
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-2 text-gray-700">
-          <UserProfile name="John Doe" role="admin" avatar="user.jpg" />
-          <span className="flex items-center gap-1 cursor-pointer">
-            English
-          </span>
+          <UserProfile role="admin" avatar="user.jpg" name="John Doe" />
+          <ChangeLanguage />
+          <input
+            type="text"
+            placeholder=". . . Search"
+            className="px-2 py-1 border rounded-xl border-gray-300 text-left"
+          />
         </div>
         <div className="flex items-center gap-4 relative">
-          <NotificationIcon
-            Icon={Icons.ShoppingCart}
-            count={5}
-            color="blue-600"
-          />
-          <NotificationIcon Icon={Icons.Bell} count={5} color="red-500" />
+          {/* <NotificationIcon Icon={Icons.Bell} count={5} color="red-500" /> */}
         </div>
       </div>
       <div className="flex items-center gap-4 text-gray-600">
-        <input
-          type="text"
-          placeholder=". . . Search"
-          className="px-2 py-1 border rounded-xl border-gray-300 text-left"
-        />
         <span className="cursor-pointer">🌙</span>
+        <NotificationIcon
+          Icon={Icons.ShoppingCart}
+          count={5}
+          color="blue-600"
+        />
         <Icons.Mail className="w-5 h-5 cursor-pointer" />
-        <Icons.MessageSquare className="w-5 h-5 cursor-pointer" />
-        <Icons.CheckSquare className="w-5 h-5 cursor-pointer" />
+        {/* <Icons.MessageSquare className="w-5 h-5 cursor-pointer" /> */}
+        {/* <Icons.CheckSquare className="w-5 h-5 cursor-pointer" /> */}
         <Icons.Calendar className="w-5 h-5 cursor-pointer" />
         <Icons.Star className="w-5 h-5 text-yellow-500 cursor-pointer" />
       </div>
